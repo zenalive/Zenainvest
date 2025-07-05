63ca1dbbf7b58b873c227667dc523fa698c44d0b
zenalive <jh2017vs@gmail.com>
2025-07-05 00:33:36 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Bem-vindo à ZenaInvest 🚀</h1>
+      <h1>Bem-vindo à ZenaInvest 🧠</h1>
       <p>Plataforma de investimentos automatizados 100% online!</p>
     </div>
   );
