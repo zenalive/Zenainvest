c4a367c1a1abcec1cf78c4a490bed07ce7543846
zenalive <jh2017vs@gmail.com>
2025-07-05 03:27:38 -0300

    Update zenabot.js

--- backend/zenabot.js
+++ backend/zenabot.js
@@ -5,7 +5,9 @@ dotenv.config();
 const binance = new Binance().options({
   APIKEY: process.env.BINANCE_API_KEY,
   APISECRET: process.env.BINANCE_API_SECRET,
+  proxy: `http://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@${process.env.PROXY_HOST}:${process.env.PROXY_PORT}`
 });
+
 
 const SYMBOL = 'BTCUSDT';
 const MODO = 'conservador'; // ou 'agressivo'
